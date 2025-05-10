2a14e09f265e07d1974feb82c836cb3a69502561
Rafał Bastek <rafun2014@gmail.com>
2025-05-10 12:32:03 +0200

    Saving progress main.py

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import logging
+import sys
 logging.basicConfig(level=logging.DEBUG)
 
 def kalkulator():
+    
     operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
-    a = float(input("Podaj wartość pierwszej liczby:"))
-    b = float(input("Podaj wartość drugiej liczby:"))
+
+    a = input("Podaj wartość pierwszej liczby:")
+    if isinstance(a, float):
+        pass
+    elif isinstance(a, int):
+        a = float(a)
+    else:
+        logging.error("Podaj właściwą wartość!")
+        sys.exit(1)
+
+    b = input("Podaj wartość drugiej liczby:")
+    if isinstance(b, float):
+        pass
+    elif isinstance(b,int):
+        b = float(b)
+    else:
+        logging.error("Podaj właściwą wartość!")
+        sys.exit(1)
+
     if operation == 1: #operacja dodawania
         logging.info("Dodaję %.2f i %.2f" % (a,b))
         print ("Wynik to %.2f" % (a+b))
@@ -19,5 +38,6 @@ def kalkulator():
         print("Wynik to %.2f" % (a/b))
     else:
         logging.error("Podaj prawidłową wartość!")
+        exit(1)
     
 print(kalkulator())
